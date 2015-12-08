f559fdc6f257ac66818d44faddfdfe9ceff3e501
Eetu Mäkelä <eetu.makela@iki.fi>
2015-12-08 17:17:42 +0200

    initial import

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('build', function() {
+  var tsProject = $.typescript.createProject('tsconfig.json');
+  var tsResult = tsProject.src()
+    .pipe($.plumber({ errorHandler: $.notify.onError("<%= error.stack %>") }))
+    .pipe($.sourcemaps.init())
+    .pipe($.typescript(tsProject));
+  return require('merge2')(
+    tsResult.js
+      .pipe($.typescriptAngular({ moduleName: 'fi.seco.sparql' }))
+      .pipe($.sourcemaps.write('.'))
+    ,
+    tsResult.dts).pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'], function() {
+  $.watch("src/**/*.ts", function() { gulp.start('build'); });
+});
+
+gulp.task('clean', function(cb) {
+  return require('del')(['dist'], cb);
+});
+
+require('gulp').task('default', ['build'])
